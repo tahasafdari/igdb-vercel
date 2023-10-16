d8e895b734535be40ed2d39a861b7ed961a3015c
Hasan Safdari <41860385+tahasafdari@users.noreply.github.com>
2023-10-16 15:25:24 +0300

    next config modified

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {}
 
 module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "http://harmonia.ink:5001/file/",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
+
   nextConfig,
   images: {
     remotePatterns: [
@@ -15,4 +30,5 @@ module.exports = {
         }
       ]
   }
+  
 }
